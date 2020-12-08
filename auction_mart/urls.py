53f85205c4e66c38b3a56d9a58fb73c0af2b4ae7
Sharjeel Qadeer <sqadeer@csquareonline.com>
2020-12-08 19:09:04 +0500

    Login and Registration

--- auction_mart/urls.py
+++ auction_mart/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^login/$', AccountLoginView.as_view(template_name="common/login.html"), name='login'),
     url(r'^signup/$', AccountSignup.as_view(template_name="common/signup.html"),  name='signup'),
     url(r'^admin/', admin.site.urls),
+    
 ]
